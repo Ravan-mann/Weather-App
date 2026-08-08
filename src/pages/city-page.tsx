fd587e918a55e0133774b9fba6fe87c709c135a2
mann <mannbhosle@gmail.com>
2026-08-08 16:47:03 +0530

    ✨ add city page weather UI and error handling
    - Render current weather, hourly temp, details, and forecast
    - Show alert on fetch error and skeleton while loading
    - Add placeholder use‑favorit hook for future favorites feature

--- src/pages/city-page.tsx
+++ src/pages/city-page.tsx
@@ -1,8 +1,61 @@
+import CurrentWeather from '@/components/currentWeather';
+import HourlyTemperature from '@/components/hourly-temprature';
+import { WeatherSkeleton } from '@/components/loading-skeleton';
+import { Alert, AlertDescription, AlertTitle } from '@/components/ui/alert';
+import { WeatherDetails } from '@/components/weather-details';
+import { WeatherForecast } from '@/components/weather-forecast';
+import { useWeatherQuery, useForecastQuery } from '@/hooks/use-weather';
+import { AlertTriangle} from 'lucide-react';
+import  {useParams, useSearchParams} from 'react-router-dom'
 const CityPage = () => {
+    const [SearchParamas] = useSearchParams();
+    const params = useParams();
+    const lat = parseFloat(SearchParamas.get("lat")||"0");
+    const lon = parseFloat(SearchParamas.get("lon")||"0");
+
+    const coordinates = {lon,lat};
+
+    const weatherQuery = useWeatherQuery(coordinates);
+    const forecastQuery = useForecastQuery(coordinates);
+
+    if (weatherQuery.error || forecastQuery.error) {
     return (
-        <div>
-            City Page
+      <Alert variant="destructive">
+        <AlertTriangle className="h-4 w-4" />
+        <AlertTitle>Error</AlertTitle>
+        <AlertDescription className="flex flex-col gap-4">
+            Failed to load the Data. plz try again 
+        </AlertDescription>
+      </Alert>
+    );
+    }
+      if (!weatherQuery.data || !forecastQuery.data || !params.cityName) {
+    return <WeatherSkeleton />;
+  }
+    return (
+ <div className="space-y-4">
+       {/* <FavoriteCities /> */}
+      <div className="flex items-center justify-between">
+        <h1 className="text-2xl font-bold tracking-tight">
+        {params.cityName},{weatherQuery.data.sys.country}
+        </h1>
+        {/* <div>favorite button</div> */}
+      </div> 
+
+      <div className="grid gap-6">
+        <div className="flex flex-col gap-4">
+          <CurrentWeather
+            data={weatherQuery.data}
+          />
+          <HourlyTemperature data={forecastQuery.data} />
         </div>
+
+        <div className="grid gap-6 md:grid-cols-2 items-start">
+          <WeatherDetails data={weatherQuery.data} />
+          <WeatherForecast data={forecastQuery.data} /> 
+        </div>
+      </div>
+    </div>
     );
 }
 export default CityPage;
